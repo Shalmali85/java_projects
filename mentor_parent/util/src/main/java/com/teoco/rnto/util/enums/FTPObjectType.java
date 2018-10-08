ed9535237639f4cd0c35058cec62b2a8c98937a6
d867421 <shalmali.roy@team.telstra.com>
2018-10-08 16:05:56 +1100

    regrouped project

--- mentor_parent/util/src/main/java/com/teoco/rnto/util/enums/FTPObjectType.java
+++
@@ -1,9 +0,0 @@
-package com.teoco.rnto.util.enums;
-
-/**
- * Types of objects on a ftp site.
- */
-public enum FTPObjectType {
-    FILE,
-    DIRECTORY
-}
--- mentor_parent/util/src/main/java/com/teoco/rnto/util/enums/LegIndices.java
+++
@@ -1,9 +0,0 @@
-package com.teoco.rnto.util.enums;
-
-/**
- * Created by guptaam on 11/19/2014.
- */
-public class LegIndices {
-    public static final int SCELL = 0;
-    public static final int NCELL = 1;
-}
